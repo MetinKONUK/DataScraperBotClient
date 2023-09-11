118260c8257c0246932f083da82a82b3b99580ba
Metin KONUK <metinkonuk@hotmail.com>
2023-09-12 00:08:15 +0300

    design DataGrid component to render fetched data

---
+++ src/Components/DataGrid.jsx
@@ -0,0 +1,90 @@
+import { Box } from '@mui/material'
+import { DataGrid } from '@mui/x-data-grid'
+import { toast } from 'react-toastify'
+
+const columns = [
+    {
+        field: 'id',
+        headerName: 'ID',
+        width: 200,
+        headerAlign: 'center',
+        align: 'center',
+    },
+    {
+        field: 'companyName',
+        headerName: 'Company Name',
+        width: 200,
+        headerAlign: 'center',
+        align: 'center',
+    },
+    {
+        field: 'professions',
+        headerName: 'Professions',
+        width: 200,
+        headerAlign: 'center',
+        align: 'center',
+    },
+    {
+        field: 'address',
+        headerName: 'Address',
+        width: 200,
+        headerAlign: 'center',
+        align: 'center',
+    },
+    {
+        field: 'primaryPhone',
+        headerName: 'Primary Phone',
+        width: 200,
+        headerAlign: 'center',
+        align: 'center',
+    },
+    {
+        field: 'secondaryPhone',
+        headerName: 'Secondary Phone',
+        width: 200,
+        headerAlign: 'center',
+        align: 'center',
+    },
+    {
+        field: 'websiteLink',
+        headerName: 'Website Link',
+        width: 200,
+        headerAlign: 'center',
+        align: 'center',
+    },
+    {
+        field: 'email',
+        headerName: 'Email',
+        width: 200,
+        headerAlign: 'center',
+        align: 'center',
+    },
+    {
+        field: 'instagram',
+        headerName: 'Instagram',
+        width: 200,
+        headerAlign: 'center',
+        align: 'center',
+    },
+]
+
+const DataGridComponent = ({ loading, data }) => {
+    return (
+        <Box style={{ width: '90%' }}>
+            <DataGrid
+                loading={loading}
+                rows={data}
+                columns={columns}
+                rowsPerPage={6}
+                autoHeight
+                checkboxSelection
+                getRowHeight={() => 'auto'}
+                density="comfortable"
+                disableRowSelectionOnClick
+                onClipboardCopy={() => toast.success('Copied to clipboard')}
+            />
+        </Box>
+    )
+}
+
+export default DataGridComponent
