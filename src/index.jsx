3775c6c562954c8f287c220727532a456268b0a6
Metin KONUK <metinkonuk@hotmail.com>
2023-09-12 00:09:04 +0300

    wrap main component with react-toastify wrapper component

---
+++ src/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import './index.css'
+import MainComponent from './MainComponent.jsx'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <>
+        <MainComponent />
+        <ToastContainer
+            position="bottom-left"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+        />
+    </>
+)
